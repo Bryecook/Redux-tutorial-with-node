43f8dad3096e47f921f650592fda117add1697a8
Bryecook <brybcook@gmail.com>
2021-07-12 13:17:39 -0500

    lodash tests

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+import { compose, pipe } from 'lodash/fp/';
+
+
+let input = "   JavaScript   ";
+
+const trim = str => str.tim();
+const wrapInDiv = str => `<div>${str}</div>`;
+const toLowerCase = str => str.toLowerCase();
+
+const transform = pipe(trim, toLowerCase, wrapInDiv);
+transform(input)
